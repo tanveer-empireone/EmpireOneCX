6e7f4ddb7611024fe4d921e6767eced18aa21bfe
Tanveer Hussain <tanvirrind@gmail.com>
2026-08-04 15:06:47 +0500

    Force non-www canonical URLs

--- inc/header.php
+++ inc/header.php
@@ -66,7 +66,7 @@ if (empty($languageAlternates)) {
 
         // Dynamic Canonical URL (Strips tracking parameters like ?gclid=123 for pure SEO indexation)
         $clean_uri = strtok($_SERVER['REQUEST_URI'], '?');
-        $canonical = "https://" . $_SERVER['HTTP_HOST'] . $clean_uri;
+        $canonical = "https://empireonecx.com" . $clean_uri;
     ?>
 
     <title><?php echo htmlspecialchars($title ?? 'EmpireOneCX', ENT_QUOTES, 'UTF-8'); ?></title>
